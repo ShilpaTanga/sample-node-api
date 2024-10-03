c3bb8adc9371c086bbed047c41b3a19ca37941d0
Shilpa Tanga <shilpatanga0@gmail.com>
2024-10-03 22:13:54 +0530

    previous version

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Stop and remove any existing container named 'sample-node-api'
-echo "Stopping and removing any existing container named 'sample-node-api'..."
-docker stop sample-node-api || true
-docker rm sample-node-api || true
+#echo "Stopping and removing any existing container named 'sample-node-api'..."
+#docker stop sample-node-api || true
+#docker rm sample-node-api || true
